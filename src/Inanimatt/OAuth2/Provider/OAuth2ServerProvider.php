b43334d528b41216d359216003d7f2c47f392123
Matt Robinson <matt@inanimatt.com>
2013-05-30 17:53:51 +0100

    Refactor OAuth2 routes and services into a silex provider

---
+++ src/Inanimatt/OAuth2/Provider/OAuth2ServerProvider.php
@@ -0,0 +1,82 @@
+<?php
+namespace Inanimatt\OAuth2\Provider;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Silex\ControllerProviderInterface;
+
+class OAuth2ServerProvider implements ServiceProviderInterface, ControllerProviderInterface
+{
+    public function register(Application $app)
+    {
+
+        // Request and response classes
+        $app['oauth2.request'] = $app->share(function () {
+            return \Inanimatt\OAuth2\Server\Util\Request::buildFromGlobals();
+        });
+
+        // Resource server (the API)
+        $app['oauth2.resource_server'] = $app->share(function () use ($app) {
+            return new \League\OAuth2\Server\Resource(
+                new \Inanimatt\OAuth2\Server\Storage\DBAL\Session($app['db'])
+            );
+        });
+
+        /**
+         * Authorization server (hands out tokens after authentication, grants
+         * access, authenticates client_credentials grants)
+         */
+        $app['oauth2.authorization_server'] = $app->share(function () use ($app) {
+            $server = new \League\OAuth2\Server\Authorization(
+                new \Inanimatt\OAuth2\Server\Storage\DBAL\Client($app['db']),
+                new \Inanimatt\OAuth2\Server\Storage\DBAL\Session($app['db']),
+                new \Inanimatt\OAuth2\Server\Storage\DBAL\Scope($app['db'])
+            );
+
+            $server->addGrantType(new \League\OAuth2\Server\Grant\ClientCredentials($server));
+            $server->setRequest($app['oauth2.request']);
+
+            return $server;
+        });
+
+        // Register authorisation route middleware
+        $app['oauth2.check_token'] = $app->share(function () use ($app) {
+
+            return function (\Symfony\Component\HttpFoundation\Request $request) use ($app) {
+                $server = $app['oauth2.resource_server'];
+
+                // Test for token existance and validity
+                try {
+                    $server->isValid();
+                }
+
+                // The access token is missing or invalid...
+                catch (\League\OAuth2\Server\Exception\InvalidAccessTokenException $e)
+                {
+                    $error = array(
+                        'error' => $e->getMessage(),
+                    );
+
+                    return $app->json($error, 403);
+                }
+            };
+        });
+    }
+
+    public function boot(Application $app) {}
+
+    public function connect(Application $app)
+    {
+        $controllers = $app['controllers_factory'];
+
+        $controllers->post('/oauth/v2/token', function () use ($app) {
+
+            $result = $app['oauth2.authorization_server']->issueAccessToken();
+
+            return $app->json($result);
+        });
+
+        return $controllers;
+    }
+
+}
--- src/app.php
+++ src/app.php
@@ -2,100 +2,32 @@
 require __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application;
-$app['debug'] = true;
-
-// Initiate the Request handler
-$app['oauth_request'] = $app->share(function () {
-    return Inanimatt\OAuth2\Server\Util\Request::buildFromGlobals();
-});
-
-$app['json_response'] = function () {
-    return new \Symfony\Component\HttpFoundation\JsonResponse;
-};
+$app['debug'] = false;
 
 // Initiate a new database connection
 $app['db'] = $app->share(function () {
     $config = new \Doctrine\DBAL\Configuration();
-    $connectionParams = array(
-        'dbname'   => 'hubapi_slim',
-        'user'     => 'root',
-        'password' => '',
-        'host'     => 'localhost',
-        'driver'   => 'pdo_mysql',
-        'encoding' => 'utf8',
-    );
+    $connectionParams = require __DIR__.'/database.php';
 
     $conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
 
     return $conn;
 });
 
-// Initiate the auth server with the models
-$app['oauth_server'] = $app->share(function () use ($app) {
-    return new League\OAuth2\Server\Resource(
-        new Inanimatt\OAuth2\Server\Storage\DBAL\Session($app['db'])
-    );
-});
+// Register OAuth2 server and controllers
+$oauth2_service_provider = new Inanimatt\OAuth2\Provider\OAuth2ServerProvider();
+$app->register($oauth2_service_provider);
+$app->mount('/', $oauth2_service_provider);
 
-$app['auth_server'] = $app->share(function () use ($app) {
-    $server = new League\OAuth2\Server\Authorization(
-        new Inanimatt\OAuth2\Server\Storage\DBAL\Client($app['db']),
-        new Inanimatt\OAuth2\Server\Storage\DBAL\Session($app['db']),
-        new Inanimatt\OAuth2\Server\Storage\DBAL\Scope($app['db'])
-    );
-
-    $server->addGrantType(new League\OAuth2\Server\Grant\ClientCredentials($server));
-    $server->setRequest($app['oauth_request']);
-
-    return $server;
-});
-
-$app['check_token'] = $app->share(function () use ($app) {
-
-    return function (Symfony\Component\HttpFoundation\Request $request) use ($app) {
-        $server = $app['oauth_server'];
-
-        // Test for token existance and validity
-        try {
-            $server->isValid();
-        }
-
-        // The access token is missing or invalid...
-        catch (League\OAuth2\Server\Exception\InvalidAccessTokenException $e)
-        {
-            $response = $app['json_response'];
-            $response->setData(array(
-                'error' => $e->getMessage(),
-            ));
-            $response->setStatusCode(403);
-
-            return $response;
-        }
-    };
-
-});
-
-$app->post('/oauth/v2/token', function () use ($app) {
-
-    $result = $app['auth_server']->issueAccessToken();
-
-    $response = $app['json_response'];
-    $response->setData($result);
-
-    return $response;
-});
-
-$app->get('/api/assets', function () use ($app) {
+// Define API routes
+$app->get('/api/test', function () use ($app) {
     $result = array(
         'result' => 'Hello world',
-        'client_id' => $app['oauth_server']->getClientId(),
-        'scopes' => $app['oauth_server']->getScopes(),
+        'client_id' => $app['oauth2.resource_server']->getClientId(),
+        'scopes' => $app['oauth2.resource_server']->getScopes(),
     );
 
-    $response = $app['json_response'];
-    $response->setData($result);
-
-    return $response;
-})->before($app['check_token']);
+    return $app->json($result);
+})->before($app['oauth2.check_token']);
 
 return $app;
